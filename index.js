7d64e258446938308e58d4bf89fc118ea1dc395c
Thapelo Mpandle <ThapeloMpandle@outlook.com>
2026-01-23 09:54:24 +0200

    Hardening: add guarded Brevo sender with timeout, respond before outbound sends, add request logger and SMTP timeouts

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /* Cleaned index.js: single Brevo require, fixed /api/quote handler, timeout wrapper */
 const { sendViaBrevo } = require('./brevo-send');
 const sendQuoteEmailVerbose = require('./verbose-email.js');
